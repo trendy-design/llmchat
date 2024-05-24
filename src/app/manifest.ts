e56b125360a83d58bfc42ce88f9c2a23cd2eb39b
hello@deeplakhani.co <hello@deeplakhani.co>
2024-05-24 15:36:08 +0530

    chore: add pwa icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,9 +11,24 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#1d1d1d",
     icons: [
       {
-        src: "/favicon.ico",
-        sizes: "any",
-        type: "image/x-icon",
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-256x256.png",
+        sizes: "256x256",
+        type: "image/png",
+      },
+      {
+        src: "/icon-384x384.png",
+        sizes: "384x384",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
     ],
   };
