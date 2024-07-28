627eb4a1b1feb72e4287c42d4ae785599b85ae07
Deep <hello@deeplakhani.co>
2024-07-28 14:58:46 -0400

    update ratelimit size

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 const ratelimit = new Ratelimit({
   redis: kv,
   // 5 requests from the same IP in 10 seconds
-  limiter: Ratelimit.slidingWindow(5, "10 s"),
+  limiter: Ratelimit.slidingWindow(5, "1 d"),
 });
 
 export const config = {
