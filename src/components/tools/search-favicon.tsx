e9e34df08f39733c9ecf99747c1d27cd5f5ff36f
Deep <hello@deeplakhani.co>
2024-07-30 14:37:10 -0400

    fix link card

--- src/components/tools/search-favicon.tsx
+++ src/components/tools/search-favicon.tsx
@@ -1,16 +1,22 @@
+import { cn } from "@/helper/clsx";
 import { Globe02Icon } from "@hugeicons/react";
 import Image from "next/image";
 import { FC, useState } from "react";
 
 export type TSeachFavicon = {
   link: string;
+  className?: string;
 };
 
-export const SearchFavicon: FC<TSeachFavicon> = ({ link }) => {
+export const SearchFavicon: FC<TSeachFavicon> = ({ link, className }) => {
   const [error, setError] = useState<boolean>(false);
   if (error) {
     return (
-      <Globe02Icon size={14} strokeWidth={1.5} className="text-gray-500" />
+      <Globe02Icon
+        size={14}
+        strokeWidth={1.5}
+        className={cn("text-gray-500", className)}
+      />
     );
   }
   return (
@@ -22,7 +28,7 @@ export const SearchFavicon: FC<TSeachFavicon> = ({ link }) => {
       }}
       width={0}
       height={0}
-      className="h-4 w-4 rounded-sm object-cover"
+      className={cn("h-4 w-4 rounded-sm object-cover", className)}
       sizes="70vw"
     />
   );
