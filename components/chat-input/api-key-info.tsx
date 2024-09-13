eeddf89e336709fa1bcc856d916998ddba507416
Deep <hello@deeplakhani.co>
2024-09-13 13:44:49 -0400

    refactor: improve the api key message

--- components/chat-input/api-key-info.tsx
+++ components/chat-input/api-key-info.tsx
@@ -1,37 +1,47 @@
+import { useAssistantUtils } from "@/hooks";
 import { usePreferenceContext } from "@/libs/context";
 import { Button, Flex, Type } from "@/ui";
 import Link from "next/link";
 
 export const ApiKeyInfo = () => {
   const { apiKeys, preferences } = usePreferenceContext();
+  const { getAssistantByKey } = useAssistantUtils();
+
+  const assistant = getAssistantByKey(preferences.defaultAssistant);
 
   const hasApiKeys =
     apiKeys.filter(
-      (key) =>
-        ["openai", "anthropic", "gemini", "groq"].includes(key.provider) &&
-        key.key,
+      (key) => assistant?.model.provider === key.provider && key.key,
     ).length > 0;
 
-  if (hasApiKeys) {
-    return null;
-  }
   if (preferences.defaultAssistant === "llmchat") {
     return (
       <Flex className="w-full py-1 pl-3 pr-1" justify="between" items="center">
         <Type size="xs" textColor="secondary">
-          LLMChat is free to use with limited access, bring your own API key for
-          unlimited access.
+          LLMChat: Free with limits in public beta. Add API keys for unlimited
+          use.
         </Type>
-        <Button variant="link" size="xs" className="text-teal-600">
-          Manage API Keys
+        <Button
+          variant="link"
+          size="xs"
+          className="text-teal-600"
+          onClick={() => {
+            window.location.href = window.location.origin + "/settings/llms/";
+          }}
+        >
+          Add API Keys
         </Button>
       </Flex>
     );
   }
+
+  if (hasApiKeys) {
+    return null;
+  }
   return (
     <Flex className="w-full py-1 pl-3 pr-1" justify="between" items="center">
       <Type size="xs" textColor="secondary">
-        Use your own API key or try{" "}
+        Use your own {assistant?.model.provider} API key or try{" "}
         <Link
           href={window.location.origin + "/settings/llms/ollama"}
           className="inline-block px-1 underline decoration-zinc-500/20 underline-offset-2"
@@ -45,10 +55,13 @@ export const ApiKeyInfo = () => {
         size="xs"
         className="text-teal-600"
         onClick={() => {
-          window.location.href = window.location.origin + "/settings/llms";
+          window.location.href =
+            window.location.origin +
+            "/settings/llms/" +
+            assistant?.model.provider;
         }}
       >
-        Manage API Keys
+        Add API Key
       </Button>
     </Flex>
   );
