b3aa3c108d87496723666fdef5586968c3cd2c13
Deep <hello@deeplakhani.co>
2025-03-29 19:35:11 -0400

    remove next config

--- apps/api/next.config.ts
+++ apps/api/next.config.ts
@@ -1,30 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-    /* config options here */
-    async headers() {
-        return [
-            {
-                // matching all API routes
-                source: '/api/:path*',
-                headers: [
-                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: process.env.NEXT_PUBLIC_APP_URL || 'no-origin',
-                    }, // replace this your actual origin
-                    {
-                        key: 'Access-Control-Allow-Methods',
-                        value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Headers',
-                        value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization',
-                    },
-                ],
-            },
-        ];
-    },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
