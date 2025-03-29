b2f6799c3cdd61953c2153024742a868f4563f31
Deep <hello@deeplakhani.co>
2025-03-29 19:04:17 -0400

    allowed options

--- apps/api/next.config.ts
+++ apps/api/next.config.ts
@@ -13,7 +13,10 @@ const nextConfig: NextConfig = {
                         key: 'Access-Control-Allow-Origin',
                         value: process.env.NEXT_PUBLIC_APP_URL || 'no-origin',
                     }, // replace this your actual origin
-                    { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS',
+                    },
                     {
                         key: 'Access-Control-Allow-Headers',
                         value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization',
