61bbc17e37194ecd5bc95348103f02789372f8be
Deep <hello@deeplakhani.co>
2025-04-06 02:37:08 -0400

    fixing prisma init issue

--- packages/prisma/client.ts
+++ packages/prisma/client.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+declare global {
+    // eslint-disable-next-line no-var
+    var prisma: PrismaClient | undefined;
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
