09c76ae18f76af001cb1ffa0c61b7f5fb8b51a90
Deep <hello@deeplakhani.co>
2025-04-08 00:04:37 -0400

    disable auto capture

--- packages/common/context/posthog.tsx
+++ packages/common/context/posthog.tsx
@@ -9,7 +9,8 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
     useEffect(() => {
         posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
             api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-            capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+            capture_pageview: false,
+            autocapture: false, // Disable automatic pageview capture, as we capture manually
         });
     }, []);
 
